80f1064a999a8bed6afd1ba02852ffa5e4e1992d
Monica Dixon <monica.dixon@bounteous.com>
2021-01-21 16:49:32 -0600

    added back collage installation images

--- src/images/artLargeCollage.js
+++ src/images/artLargeCollage.js
@@ -1,26 +1,26 @@
-// import One from '../images/artwork/LargeCollage/Bedlam.jpg'
-// import Two from '../images/artwork/LargeCollage/IMG_5704.jpg'
-// import Three from '../images/artwork/LargeCollage/StreetcarCeiling.jpg'
+import One from '../images/artwork/LargeCollage/Bedlam.jpg'
+import Two from '../images/artwork/LargeCollage/IMG_5704.jpg'
+import Three from '../images/artwork/LargeCollage/StreetcarCeiling.jpg'
 
 
-// export const photos = [
+export const photos = [
 
-//       {
-//         src: One,
-//         width: 3,
-//         height: 2,
-//         title: "'Bedlam', Permanant collection at the Crossroads Hotel in Kansas City, 2018. Photo courtesy of the Crossroads Hotel."
-//       },
-//       {
-//         src: Two,
-//         width: 1,
-//         height: 1.25,
-//         title: "'Celestial Heap' installed on the ceiling of one of the Kansas City street cars, 2018"
-//       },
-//       {
-//         src: Three,
-//         width: 1,
-//         height: 1.25,
-//         title: "'Celestial Heap' installed on the ceiling of one of the Kansas City street cars, 2018"
-//       },
-//   ];
+      {
+        src: One,
+        width: 3,
+        height: 2,
+        title: "'Bedlam', Permanant collection at the Crossroads Hotel in Kansas City, 2018. Photo courtesy of the Crossroads Hotel."
+      },
+      {
+        src: Two,
+        width: 1,
+        height: 1.25,
+        title: "'Celestial Heap' installed on the ceiling of one of the Kansas City street cars, 2018"
+      },
+      {
+        src: Three,
+        width: 1,
+        height: 1.25,
+        title: "'Celestial Heap' installed on the ceiling of one of the Kansas City street cars, 2018"
+      },
+  ];
